a83399a5910b349eef4ebca831ec43c16efff604
euru306 <grado.dragon83@ezweb.ne.jp>
2025-05-02 13:34:57 +0900

    Add files via upload
    再提出

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,20 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	private int gear = 1;
+	private int speed = 10;
+	
+	public void gearChange(int newGear) {
+		int oldGear = gear;
+		gear = newGear;
+		
+		if (gear >= 1&& gear <= 5) {
+		speed = gear * 10; // ギア × 10 で速度計算
+		} else {
+			speed = 10; // 無効なギアは10km/h固定
+		}
+		System.out.println("ギアが" + oldGear + "から" + gear + "に変わりました");
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+}
