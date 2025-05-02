f9d1d5211be5e29617ba5aa131c5f39480b5ff2a
euru306 <grado.dragon83@ezweb.ne.jp>
2025-05-02 23:17:42 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+    public KatoHanako_Chapter18() {
+        this.setGivenName();
+        this.commonIntroduce();
+    }
+
+    private void setGivenName() {
+        givenName = "花子";
+    }
+
+   
+    public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
